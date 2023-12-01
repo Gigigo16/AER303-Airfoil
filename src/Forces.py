32771cd245d4be22dffa1ba5c3fe8cfe3228ee8e
Rodrigo Salazar <rodrigo.salazar@mail.utoronto.ca>
2023-12-01 01:27:39 -0500

    Airfoil Parser created

--- src/Forces.py
+++ src/Forces.py
@@ -1,7 +1,29 @@
-from scipy import integrat
-
+from scipy import integrate
 import pandas as pd
 import numpy as np
+import math
 
-def NormalForce(p_top: np.array, p_bot: np.array):
-    
+def NormalForce(p_top: np.array, p_bot: np.array, top_p_pos: list, bot_p_pos: list, airfoil: np.array):
+    '''
+    Returns the Normal force for pressure distribution.
+
+    Parameters:
+    -----------   
+    p_top : np.array
+        top airfoil pressure distribution
+    p_bot : np.array
+        bottom airfoil pressure distribution
+    top_p_pos : list
+        top airfoil pressure tap positions
+    bot_p_pos : list
+        bottom airfoil pressure tap positions
+
+    Returns:
+    --------
+    N: Normal Force
+    dN: Normal Force uncertainty
+    '''
+    for i in range(1,len(top_p_pos)):
+        math.acos()
+
+    integrate.trapezoid()
--- src/main.py
+++ src/main.py
@@ -7,8 +7,10 @@ from scipy import io
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import csv
 from ReynoldsNumber import *
 #from Forces import *
+
 
 # DEFINITIONS
 ########################
@@ -19,11 +21,28 @@ air_bot_tap_pos = [0.90, 0.60, 0.40, 0.30, 0.20, 0.10, 0.05]
 # Angles of Attack
 alpha = [0, 4, 6, 8, 9, 10, 11, 12, 14, 15, 17]
 
+# Loading Airfoil Coordinates
+##############################
+with open(".\data\Clark_Y_Airfoil.csv", newline='') as f:
+    points = csv.reader(f, delimiter=';')
+    data = list(points)
+
+is_top = True
+airfoil_top = []
+airfoil_bot = []
+for i in range(1,len(data)):
+    if i != 0 and float(data[i][0]) == float(0.0):
+        is_top = False
+    if is_top:
+        airfoil_top.append([float(data[i][0]), float(data[i][1])])
+    else:
+        airfoil_bot.append([float(data[i][0]), float(data[i][1])])
+
+    
 for i in alpha:
     data = io.loadmat(".\data\Filtered\F-Experimental_data_%d.mat"%i)
     p_foil_top = data['p_airfoil'][0][0:12]
     p_foil_bot = data['p_airfoil'][0][12:18]
-
 
 Re = ReynoldsNumber(L=0.1)
 
