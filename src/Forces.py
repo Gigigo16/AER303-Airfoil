d516e819b188a292bcf2d9e3c8dd9efc00594a9f
Rodrigo Salazar <gigigo.yeye@gmail.com>
2023-12-03 00:17:19 -0500

    Moment forces function added

--- src/Forces.py
+++ src/Forces.py
@@ -154,3 +154,53 @@ def PressureDragForce(alpha: float, dalpha: float, N: float, dN: float, A: float
     dDp =  np.sqrt((np.sin(np.deg2rad(alpha)) * dN)**2 + (np.cos(np.deg2rad(alpha)) * dA)**2 + ((N*np.cos(np.deg2rad(alpha)) - A*np.sin(np.deg2rad(alpha)))*np.deg2rad(alpha))**2)
 
     return Dp, dDp
+
+def MomentLE(p_top: np.array, p_bot: np.array, p_err_bot: np.array, p_err_top: np.array, top_p_pos: np.array, bot_p_pos: np.array):
+    '''
+    Returns the Moment about the leading edge 
+    acting on the airfoil for given pressure distribution.
+
+    Parameters:
+    -----------   
+    p_top : np.array
+        top airfoil pressure distribution
+    p_bot : np.array
+        bottom airfoil pressure distribution
+    p_err_top : np.array
+        top airfoil perssure error
+    p_err_bot : np.array
+        bottom airfoil perssure error
+    top_p_pos : list
+        top airfoil pressure tap positions [x; y]
+    bot_p_pos : list
+        bottom airfoil pressure tap positions [x; y]
+    Returns:
+    --------
+    M: Moment 
+    dM: Moment uncertainty
+    '''
+    x_upper = top_p_pos[:, 0]
+    x_lower = bot_p_pos[:, 0]
+    y_upper = top_p_pos[:, 1]
+    y_lower = bot_p_pos[:, 1]
+    
+    theta_upper= np.arctan(np.diff(y_upper)/np.diff(x_upper))
+    theta_lower= np.arctan(np.diff(y_lower)/np.diff(x_lower))
+    
+    ds_upper = np.sqrt(np.diff(x_upper)**2 + np.diff(y_upper)**2)
+    ds_lower = np.sqrt(np.diff(x_lower)**2 + np.diff(y_lower)**2)   
+
+    for i in range(len(ds_upper)):
+        M += 0.5*(p_top[i] + p_top[i+1])*np.cos(theta_upper[i])*x_upper[i]*ds_upper[i]
+        M += -0.5*(p_top[i] + p_top[i+1])*np.sin(theta_upper[i])*y_upper[i]*ds_upper[i]
+
+        dM += (0.5*np.cos(theta_upper[i])*x_upper[i]*ds_upper[i]-0.5*np.sin(theta_upper[i])*y_upper[i]*ds_upper[i])**2
+
+    for i in range(len(ds_lower)):
+        M += 0.5*(p_bot[i] + p_bot[i+1])*np.cos(theta_lower[i])*x_lower[i]*ds_lower[i]
+        M += -0.5*(p_bot[i] + p_bot[i+1])*np.sin(theta_lower[i])*y_lower[i]*ds_lower[i]
+
+        dM += (0.5*np.cos(theta_lower[i])*x_lower[i]*ds_lower[i]-0.5*np.sin(theta_lower[i])*y_lower[i]*ds_lower[i])**2
+    
+    dM = np.sqrt(dM)
+    return M, dM
