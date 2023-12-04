01f298d5dca133658c1d13c077291ae77e3785db
Rodrigo Salazar <gigigo.yeye@gmail.com>
2023-12-03 21:41:01 -0500

    eeee

--- src/main.py
+++ src/main.py
@@ -53,7 +53,6 @@ for i in range(1,len(data)):
 airfoil_top = np.array(airfoil_top)*0.1  # Multiplying values by cord length (values given are per unit cord)
 airfoil_bot = np.array(airfoil_bot)*0.1 # Multiplying values by cord length (values given are per unit cord)
 
-
 pressure_data = []
 # PROCESSING DATA
 ########################
@@ -73,8 +72,6 @@ for i in alpha:
     p = (data['p_airfoil'][0]*gain + offset)*Hg2Pa
     pressure_data.append(list(map(float, p)))
     
-    #print((data['p_airfoil'][0]*gain + offset)*Hg2Pa)
-    #print(p_top, p_bot)
 
     p_r1_err = np.zeros_like(p_r1) #temp
     p_r2_err = np.zeros_like(p_r2) #temp
@@ -95,35 +92,10 @@ for i in alpha:
     
     #finding the dynamic freestream pressure
     q_inf, q_inf_err = DynPressure(U_inf, U_inf_err)
-    # print(q_inf)
 
     #finding the Cp distribution over the airfoil
     Cp_top, Cp_bot, Cp_top_err, Cp_bot_err = Cpressure(p_top, p_bot, p_top_err, p_bot_err, q_inf, q_inf_err)
     print((Cp_top_err))
     exit()
-    # print(Cp_top)
-
-    #print(p_bot, Cp_bot, air_bot_tap_pos)
-
-    ##TEST:
-    #y = [0, 1.67, 3.33, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16.67, 18.33, 20]
-    #plt.plot(v_r1[0], y)
-    #plt.plot(air_top_tap_pos, Cp_top)
-    #plt.plot(air_bot_tap_pos, Cp_bot)
-    #plt.gca().invert_yaxis()
-    #plt.title(i)
-    #plt.show()
 
 PressuretoCSV(alpha, np.array(pressure_data))
-
-####################################################################
-# TOO BE REMOVED BEFORE SUBMISSION TEST ONLY
-#mat = io.loadmat("data\Filtered\Experimental_data_0.mat")
-
-# p_foil_top = data['p_airfoil'][0][0:12]
-# p_foil_bot = data['p_airfoil'][0][12:19]
-
-# plt.plot(air_top_tap_pos, p_foil_top)
-# plt.plot(air_bot_tap_pos, p_foil_bot)
-# plt.show()
-####################################################################
