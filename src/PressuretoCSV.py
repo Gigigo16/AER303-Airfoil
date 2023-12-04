6c57df053372c345e8d9b245b7a71e667a519f3b
Rodrigo Salazar <rodrigo.salazar@mail.utoronto.ca>
2023-12-04 02:05:49 -0500

    added rake pressure csv function and other fixes.

--- src/PressuretoCSV.py
+++ src/PressuretoCSV.py
@@ -19,27 +19,49 @@ def PressuretoCSV(alpha: np.array, p: np.array):
     """
     for i in range(len(alpha)):
         df = pd.DataFrame(p[i].T, 
-                          index=['Port 1', 
-                                 'Port 2', 
-                                 'Port 3',
-                                 'Port 4', 
-                                 'Port 5', 
-                                 'Port 6', 
-                                 'Port 7', 
-                                 'Port 8', 
-                                 'Port 9', 
-                                 'Port 10', 
-                                 'Port 11', 
-                                 'Port 12', 
-                                 'Port 13', 
-                                 'Port 14', 
-                                 'Port 15', 
-                                 'Port 16', 
-                                 'Port 17', 
-                                 'Port 18',
-                                 'Port 19'], columns=['Pressure (Pa)'])
+                          index=['1', 
+                                 '2', 
+                                 '3',
+                                 '4', 
+                                 '5', 
+                                 '6', 
+                                 '7', 
+                                 '8', 
+                                 '9', 
+                                 '10', 
+                                 '11', 
+                                 '12', 
+                                 '13', 
+                                 '14', 
+                                 '15', 
+                                 '16', 
+                                 '17', 
+                                 '18',
+                                 '19'], columns=['Pressure (Pa)'])
+        df.index.name = 'Port #'
         
         df.to_csv('.\data\CSV\Pressure_AoA%d.csv'%alpha[i], index=True)
     return 0
-def RakePressuretoCSV(alpha: np.array, p_rake: np.array):
-    print("eee")
+
+def RakePressuretoCSV(alpha: np.array, p_rake: np.array , y: np.array):
+    """
+    exports given rake pressure data to CSV files for each AoA
+
+    Parameters
+    ----------
+    alpha : np.array
+        array containing AoA values
+    p_rake : np.array (2D)
+        array containing rake pressure data for each AoA where each row 
+        is a different AoA and each column is a different port
+    y : np.array (cm)
+        array containing rake port positions
+    Returns
+    -------
+    0
+    """
+    for i in range(len(alpha)):
+        df = pd.DataFrame(p_rake[i].T, 
+                          index=y, columns=["Position (cm)",'Pressure (Pa)'])
+        
+        df.to_csv('.\data\CSV\Rake_Pressure_AoA%d.csv'%alpha[i], index=True)
