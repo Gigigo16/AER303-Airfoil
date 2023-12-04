70cd8bcf956a7feeebe2f6285f23ad4033616597
Rodrigo Salazar <rodrigo.salazar@mail.utoronto.ca>
2023-12-04 00:22:56 -0500

    etsd

--- src/Uncertainty.py
+++ src/Uncertainty.py
@@ -38,13 +38,12 @@ def DataErr(raw_p: np.array):
      
     #find index of first root:
     lim = np.where(Bxx < 0)[0][0]
-    print(Bxx)
     #finding the integral time scale:
-    dt = 1/30000\
+    dt = 1/30000
     T = np.trapz(Bxx[:lim], dx=dt) 
     #T = sum(Bxx[:lim]) * dt
     N = len(raw_p)/(2*T) * dt
     std = np.std(raw_p)
     dP = 1.96*std/np.sqrt(N)
-
+    print(dP)
     return dP
