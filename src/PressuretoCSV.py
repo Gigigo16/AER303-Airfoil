13c8c07d6ed2377cbf62a24aa392b0be2b025b6b
Rodrigo Salazar <rodrigo.salazar@mail.utoronto.ca>
2023-12-04 01:56:38 -0500

    fixed PressuretoCSV

--- src/PressuretoCSV.py
+++ src/PressuretoCSV.py
@@ -1,18 +1,45 @@
 import numpy as np
+import pandas as pd
 
 def PressuretoCSV(alpha: np.array, p: np.array):
-    array = [[1]]
-    for i in range(0,19):
-        array[0].append(np.float32(i+1))
-    
-    j = 0
-    for i in alpha:
-        a = [i]
-        array.append(np.concatenate([a, p[j]],  axis=None))
-        j += 1
-    
-    array = np.array(array)
-    np.savetxt("./data/CSV/Pressure.csv", array, delimiter=';', fmt='%.18e')
+    """
+    exports given pressure data to CSV files for each AoA
 
+    Parameters
+    ----------
+    alpha : np.array
+        array containing AoA values
+    p : np.array (2D)
+        array containing pressure data for each AoA where each row 
+        is a different AoA and each column is a different port
+    
+    Returns
+    -------
+    0
+    """
+    for i in range(len(alpha)):
+        df = pd.DataFrame(p[i].T, 
+                          index=['Port 1', 
+                                 'Port 2', 
+                                 'Port 3',
+                                 'Port 4', 
+                                 'Port 5', 
+                                 'Port 6', 
+                                 'Port 7', 
+                                 'Port 8', 
+                                 'Port 9', 
+                                 'Port 10', 
+                                 'Port 11', 
+                                 'Port 12', 
+                                 'Port 13', 
+                                 'Port 14', 
+                                 'Port 15', 
+                                 'Port 16', 
+                                 'Port 17', 
+                                 'Port 18',
+                                 'Port 19'], columns=['Pressure (Pa)'])
+        
+        df.to_csv('.\data\CSV\Pressure_AoA%d.csv'%i, index=True)
+    return 0
 def RakePressuretoCSV(alpha: np.array, p_rake: np.array):
     print("eee")
